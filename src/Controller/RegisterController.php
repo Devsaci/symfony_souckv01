1a7cb6a9e8b524c00d2ed45fa388d0089b299f27
saci zakaria <sacideveloppeur@gmail.com>
2021-02-05 11:03:06 +0100

    <h1>INSCRIPTION</h1>

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -9,12 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class RegisterController extends AbstractController
 {
     /**
-     * @Route("/register", name="register")
+     * @Route("/inscription", name="register")
      */
     public function index(): Response
     {
         return $this->render('register/index.html.twig', [
-            'controller_name' => 'RegisterController',
+
         ]);
     }
 }
