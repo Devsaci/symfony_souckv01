9fd49d7a2ef05828cfe5c77cec427b416ba76c54
saci zakaria <sacideveloppeur@gmail.com>
2021-02-05 12:25:47 +0100

    modif

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -12,8 +12,11 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('firstname')
+            ->add('lastname'
             ->add('email')
             ->add('password')
+         )
         ;
     }
 
