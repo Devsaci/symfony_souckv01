8104796ff5d1d6e2623d483b2333c178d3ab5afe
saci zakaria <sacideveloppeur@gmail.com>
2021-02-07 21:39:07 +0100

    findOneBy

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -39,12 +40,20 @@ class ProductController extends AbstractController
 
     /**
      * @Route("/produit/{slug}", name="product")
+     * @param $slug
+     * @return RedirectResponse|Response
      */
     public function show($slug)
     {
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
+        $product = $this->entityManager->getRepository(Product::class)->findOneBy(['slug' => $slug]);
+
+        if (!$product) {
+            return $this->redirectToRoute('products');
+        }
+
+
         return $this->render('product/show.html.twig', [
-            'products' => $products
+            'product' => $product
         ]);
     }
 
