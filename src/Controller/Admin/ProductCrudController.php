3ecd4f3e896c16eb852a5e81605b6a9b35c41d3f
saci zakaria <sacideveloppeur@gmail.com>
2021-02-07 17:15:12 +0100

    33. Création des vues pour afficher nos produits à nos utilisateurs >symfony console make:controller creat product

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -4,11 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -24,6 +24,7 @@ class ProductCrudController extends AbstractCrudController
     {
         return [
             TextField::new('name'),
+            SlugField::new('slug')->setTargetFieldName('name'),
             TextField::new('subtitle'),
             ImageField::new('illustration')
                 ->setBasePath('uploads/')
