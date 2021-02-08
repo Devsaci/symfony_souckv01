a5cba250bae5c5029651ebab4e56a00884284cfc
saci zakaria <sacideveloppeur@gmail.com>
2021-02-08 14:41:53 +0100

    public function configureOptions

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -3,12 +3,21 @@
 namespace App\Form;
 
 
+use App\Classe\Search;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SearchType extends AbstractType
 {
 
 
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Search::class,
+
+        ]);
+    }
 
 
 }
