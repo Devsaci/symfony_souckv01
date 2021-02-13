304ea635ecd2d2dfef3530f56a7117564103676f
saci zakaria <sacideveloppeur@gmail.com>
2021-02-13 20:16:47 +0100

    SessionInterface

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace App\Classe;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 class Cart
 {
 
     private $session;
 
-    public function __construct()
+    public function __construct(SessionInterface $session)
     {
-
+        $this->session = $session;
     }
 
 
