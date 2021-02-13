57886758a3e48bbfb8f1ce00221ba082d8b5faf5
saci zakaria <sacideveloppeur@gmail.com>
2021-02-13 22:50:37 +0100

    add-to-cart multiple

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -16,13 +16,14 @@ class Cart
 
     public function add($id)
     {
+        $cart = $this->session->get('cart', []);
 
-        $this->session->set('cart', [
-        [
-            'id' => $id,
-            'quantity' => 1
-        ]
-        ]);
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+        $this->session->set('cart', $cart);
 
     }
 
