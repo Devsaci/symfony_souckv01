36c0864f6a40871a58211e19c9fb6e39bd7e0645
saci zakaria <sacideveloppeur@gmail.com>
2021-03-02 00:06:28 +0100

    affichage Tableau pannier avec refCompletes

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,19 +3,37 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/mon-panier", name="cart")
+     * @param Cart $cart
+     * @return Response
      */
-    public function index(Cart $cart)
+    public function index(Cart $cart): Response
     {
-//dd($cart->get());
-
+        //dd($cart->get());
+        $cartComplete= [];
+        foreach ($cart->get() as $id => $quantity ){
+                $cartComplete[]  = [
+        'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
+        'quantity' => $quantity
+                                ];
+        }
+dd($cartComplete);
         return $this->render('cart/index.html.twig', [
             'cart' => $cart->get()
         ]);
